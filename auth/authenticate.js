92bb379ba397d7b1c5a97bb18882785d6653d725
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-10-21 22:45:42 -0400

    fixed auth

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -6,7 +6,9 @@ module.exports = (req, res, next) => {
         next({ code: 401, message: "Missing Authorization header" });
     } else {
 
-        if(token) {
+        const [authType, token] = req.headers.authorization.split(" ");
+
+        if(authType && authType.toLowerCase() === 'bearer' && token) {
             jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
                 if(!err) {
                     req.jwt = decodedToken;
